8f738fd7557c208f299d77c467e475385e945a25
MayankSaxena2214 <148477562+MayankSaxena2214@users.noreply.github.com>
2024-05-05 10:12:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Vertical sum/vertical-sum.cpp
+++ Medium/Vertical sum/vertical-sum.cpp
@@ -92,38 +92,21 @@ Node *left,*right;
 */
 class Solution{
   public:
+  map<int,int>mp;
+  void helper(Node*root,int hd){
+      if(root==NULL)return;
+      mp[hd]+=root->data;
+      if(root->left)helper(root->left,hd-1);
+      if(root->right)helper(root->right,hd+1);
+  }
     vector <int> verticalSum(Node *root) {
         // add code here.
-        vector<int>v;
-        map<int,map<int,vector<int>>>nodes;
-        queue<pair<Node*,pair<int,int>>>q;
-        q.push({root,{0,0}});
-        while(!q.empty()){
-            auto front=q.front();
-            q.pop();
-            Node*top=front.first;
-            int hd=front.second.first;
-            int lvl=front.second.second;
-            nodes[hd][lvl].push_back(top->data);
-            if(top->left){
-                q.push({top->left,{hd-1,lvl+1}});
-            }
-            if(top->right){
-                q.push({top->right,{hd+1,lvl+1}});
-            }
+        vector<int>ans;
+        helper(root,0);
+        for(auto i:mp){
+            ans.push_back(i.second);
         }
-        for(auto i:nodes){
-            int sum=0;
-            for(auto j:i.second){
-                
-                for(auto k:j.second){
-                    sum+=k;
-                }
-                
-            }
-            v.push_back(sum);
-        }
-        return v;
+        return ans;
     }
 };
 
