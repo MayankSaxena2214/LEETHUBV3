4f52434a57ec8d82e2f30659135e3922ddec7420
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-13 19:09:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -0,0 +1,199 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node*nodeToParent(Node*root,int target,map<Node*,Node*>&mp){
+        if(root==NULL){
+            return NULL;
+        }
+        // mp[root]=NULL;
+        queue<Node*>q;
+        q.push(root);
+        Node*ans=NULL;
+        while(!q.empty()){
+            Node*front=q.front();
+            q.pop();
+            if(front->data==target){
+                ans=front;
+            }
+            if(front->left){
+                mp[front->left]=front;
+                q.push(front->left);
+            }
+            if(front->right){
+                mp[front->right]=front;
+                q.push(front->right);
+            }
+        }
+        return ans;
+    }
+    void solve(Node*root,map<Node*,Node*>&mp,int&ans){
+        if(root==NULL)return;
+          map<Node*,bool>visited;
+      
+        queue<Node*>q;
+        q.push(root);
+        visited[root]=true;
+        while(!q.empty()){
+            
+            bool flag=0;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node*front=q.front();
+            q.pop();
+                if(front->left && visited[front->left]==false){
+                    flag=1;
+                    visited[front->left]=true;
+                    q.push(front->left);
+                }
+                if(front->right && visited[front->right]==false){
+                    flag=1;
+                    visited[front->right]=true;
+                    q.push(front->right);
+                }
+                if(mp[front] && visited[mp[front]]==false){
+                    flag=1;
+                    visited[mp[front]]=true;
+                    q.push(mp[front]);
+                }
+            }
+            if(flag==1){
+                ans++;
+            }
+        }
+        
+    }
+    int minTime(Node* root, int target) 
+    {
+        // Your code goes here
+        map<Node*,Node*>mp;
+        Node*targetNode=nodeToParent(root,target,mp);
+      
+        int ans=0;
+        solve(targetNode,mp,ans);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
