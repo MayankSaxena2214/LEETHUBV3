49a2c1747bf21ecb33c9414e77231929ccad5a81
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-16 21:30:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+#define N 105
+using namespace std;
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    class Node{
+    public:
+    int data,i,j;
+    Node(int d,int row,int col){
+        data=d;
+        i=row;
+        j=col;
+    }
+};
+class compare{
+    public:
+    bool operator()(Node*a,Node*b){
+        return a->data>b->data;
+    }
+};
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> kArrays, int K)
+    {
+        //code here
+          vector<int>ans;
+    priority_queue<Node*,vector<Node*>,compare>pq;
+    for(int i=0;i<K;i++){
+        pq.push(new Node(kArrays[i][0],i,0));
+    }
+    while(pq.size()>0){
+        auto temp=pq.top();
+        pq.pop();
+        ans.push_back(temp->data);
+        int row=temp->i;
+        int col=temp->j;
+        if(col+1<kArrays[row].size()){
+            pq.push(new Node(kArrays[row][col+1],row,col+1));
+        }
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    }
+	return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
