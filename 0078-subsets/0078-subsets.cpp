7a080517e0e12587ccae9fec421849254d791dfe
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-24 17:44:38 +0530

    Time: 0 ms (100%), Space: 14.2 MB (17.29%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void solve(auto&nums,auto index,auto&result,auto&output){
+    void solve(auto&nums,auto index,auto&ans,auto output){
         if(index>=nums.size()){
-            result.push_back(output);
-            return ;
+            ans.push_back(output);
+            return;
         }
-        solve(nums,index+1,result,output);
+        solve(nums,index+1,ans,output);
         output.push_back(nums[index]);
-        solve(nums,index+1,result,output);
-        output.pop_back();
+        solve(nums,index+1,ans,output);
+
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>result;
+        vector<vector<int>>ans;
         vector<int>output;
-        int index=0;
-        solve(nums,index,result,output);
-        return result;
+        int  index=0;
+        solve(nums,index,ans,output);
+        return ans;
     }
 };
