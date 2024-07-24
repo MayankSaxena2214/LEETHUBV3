120cb73d5262c1d10886c031502bce3ff25df111
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-24 18:26:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -8,65 +8,69 @@ using namespace std;
 // } Driver Code Ends
 // User function template for C++
 
-class Solution{
-    public:
-    bool isSafe(int x,int y,vector < vector < int >> & arr,vector < vector < int >> & visited,int n){
-    if((x>=0 && x<n) && (y>=0 && y<n) && (visited[x][y]==0) && arr[x][y]==1){
-        return true;
+class Solution {
+  public:
+    bool isSafe(vector<vector<int>>&mat,vector<vector<int>>&visited,int x,int y){
+        int n=mat.size();
+        if((x>=0 && x<n) && (y>=0 && y<n) && (mat[x][y]==1) && (visited[x][y]==0)){
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-void solve(vector < vector < int >> & arr,vector < vector < int >> &visited,vector<string>&ans,string path,int srcx,int srcy,int n){
-    if(srcx==n-1 && srcy==n-1){
-        ans.push_back(path);
-        return;
+    void solve(vector<vector<int>>&mat,vector<string>&ans,string output,int x,int y,vector<vector<int>>&visited,int n){
+        if(x==n-1 && y==n-1){
+            ans.push_back(output);
+            return;
+        }          
+        visited[x][y]=1;
+        //DLRU
+        //Down
+        int newx,newy;
+        newx=x+1,newy=y;
+        if(isSafe(mat,visited,newx,newy)){
+            output.push_back('D');
+            solve(mat,ans,output,newx,newy,visited,n);
+            output.pop_back();
+        }
+        //Left
+        newx=x,newy=y-1;
+        if(isSafe(mat,visited,newx,newy)){
+            output.push_back('L');
+            solve(mat,ans,output,newx,newy,visited,n);
+            output.pop_back();
+        }
+        
+        //Righnt
+        newx=x,newy=y+1;
+        if(isSafe(mat,visited,newx,newy)){
+            output.push_back('R');
+            solve(mat,ans,output,newx,newy,visited,n);
+            output.pop_back();
+        }
+        
+        //Up
+        newx=x-1,newy=y;
+        if(isSafe(mat,visited,newx,newy)){
+            output.push_back('U');
+            solve(mat,ans,output,newx,newy,visited,n);
+            output.pop_back();
+        }
+        
+        
+        visited[x][y]=0;
     }
-    visited[srcx][srcy]=1;
-    //DLRU
-    //down
-    int newx=srcx+1,newy=srcy;
-    if(isSafe(newx,newy,arr,visited,n)){
-        path.push_back('D');
-        solve(arr,visited,ans,path,newx,newy,n);
-        path.pop_back();
-    }
-    //left
-    newx=srcx,newy=srcy-1;
-    if(isSafe(newx,newy,arr,visited,n)){
-        path.push_back('L');
-        solve(arr,visited,ans,path,newx,newy,n);
-        path.pop_back();
-    }
-    //Right
-    newx=srcx,newy=srcy+1;
-    if(isSafe(newx,newy,arr,visited,n)){
-        path.push_back('R');
-        solve(arr,visited,ans,path,newx,newy,n);
-        path.pop_back();
-    }
-    newx=srcx-1,newy=srcy;
-    if(isSafe(newx,newy,arr,visited,n)){
-        path.push_back('U');
-        solve(arr,visited,ans,path,newx,newy,n);
-        path.pop_back();
-    }
-
-    visited[srcx][srcy]=0;
-}
-    vector<string> findPath(vector<vector<int>> &m, int n) {
+    vector<string> findPath(vector<vector<int>> &mat) {
         // Your code goes here
-         vector<vector<int>>visited(n,vector<int>(n,0));
-    vector<string>ans;
-    string path;
-    if(m[0][0]==0)return ans;
-    int srcx=0,srcy=0;
-    solve(m,visited,ans,path,srcx,srcy,n);
-    sort(ans.begin(),ans.end());
-    return ans;
+        vector<string>ans;
+        if(mat[0][0]==0)return ans;
+        int x=0,y=0;
+        int n=mat.size();
+        string path;
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        solve(mat,ans,path,x,y,visited,n);
+        return ans;
     }
 };
-
-    
 
 
 //{ Driver Code Starts.
@@ -77,19 +81,20 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<vector<int>> m(n, vector<int> (n,0));
+        vector<vector<int>> m(n, vector<int>(n, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 cin >> m[i][j];
             }
         }
         Solution obj;
-        vector<string> result = obj.findPath(m, n);
+        vector<string> result = obj.findPath(m);
         sort(result.begin(), result.end());
         if (result.size() == 0)
             cout << -1;
         else
-            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+            for (int i = 0; i < result.size(); i++)
+                cout << result[i] << " ";
         cout << endl;
     }
     return 0;
