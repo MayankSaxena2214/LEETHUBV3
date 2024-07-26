fefd46a507138083bc0706b5d42185f637a3f037
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-26 15:29:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+        
+        
+        set<char>st;
+        string ans;
+        for(auto ch:str){
+            
+            if(ch!=' '){
+                ans+=ch;
+                st.insert(ch);
+            }
+        }
+        if(ans.length()<26)return false;
+        if(26-st.size()<=k)return true;
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
