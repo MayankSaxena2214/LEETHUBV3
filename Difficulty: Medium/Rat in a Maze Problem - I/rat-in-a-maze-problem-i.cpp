6ab5b9dab624f1eba9b3e9326235860e05a43fb1
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-30 10:44:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -10,64 +10,59 @@ using namespace std;
 
 class Solution {
   public:
-    bool isSafe(vector<vector<int>>&mat,vector<vector<int>>&visited,int x,int y){
-        int n=mat.size();
-        if((x>=0 && x<n) && (y>=0 && y<n) && (mat[x][y]==1) && (visited[x][y]==0)){
+    bool isSafe(int x,int y,vector<vector<int>> &mat,vector<vector<int>> &visited,int n){
+        if((x>=0 && x<n) && (y>=0 && y<n) && (visited[x][y]==0) && mat[x][y]==1){
             return true;
         }
         return false;
     }
-    void solve(vector<vector<int>>&mat,vector<string>&ans,string output,int x,int y,vector<vector<int>>&visited,int n){
+    void solve(vector<vector<int>> &mat,vector<vector<int>> &visited,vector<string>&ans,string path,int x,int y,int n){
         if(x==n-1 && y==n-1){
-            ans.push_back(output);
-            return;
-        }          
+            ans.push_back(path);
+            return ;
+        }
         visited[x][y]=1;
-        //DLRU
-        //Down
-        int newx,newy;
-        newx=x+1,newy=y;
-        if(isSafe(mat,visited,newx,newy)){
-            output.push_back('D');
-            solve(mat,ans,output,newx,newy,visited,n);
-            output.pop_back();
+        //D
+        int newx=x+1,newy=y;
+        if(isSafe(newx,newy,mat,visited,n)){
+            path.push_back('D');
+            solve(mat,visited,ans,path,newx,newy,n);
+            path.pop_back();
         }
-        //Left
+        //L
         newx=x,newy=y-1;
-        if(isSafe(mat,visited,newx,newy)){
-            output.push_back('L');
-            solve(mat,ans,output,newx,newy,visited,n);
-            output.pop_back();
+        if(isSafe(newx,newy,mat,visited,n)){
+            path.push_back('L');
+            solve(mat,visited,ans,path,newx,newy,n);
+            path.pop_back();
         }
-        
-        //Righnt
+        //R
         newx=x,newy=y+1;
-        if(isSafe(mat,visited,newx,newy)){
-            output.push_back('R');
-            solve(mat,ans,output,newx,newy,visited,n);
-            output.pop_back();
+        if(isSafe(newx,newy,mat,visited,n)){
+            path.push_back('R');
+            solve(mat,visited,ans,path,newx,newy,n);
+            path.pop_back();
+        }
+        newx=x-1,newy=y;
+        if(isSafe(newx,newy,mat,visited,n)){
+            path.push_back('U');
+            solve(mat,visited,ans,path,newx,newy,n);
+            path.pop_back();
         }
         
-        //Up
-        newx=x-1,newy=y;
-        if(isSafe(mat,visited,newx,newy)){
-            output.push_back('U');
-            solve(mat,ans,output,newx,newy,visited,n);
-            output.pop_back();
-        }
         
         
         visited[x][y]=0;
     }
     vector<string> findPath(vector<vector<int>> &mat) {
         // Your code goes here
+        if(mat[0][0]==0)return {};
+        int srcx=0,srcy=0;
         vector<string>ans;
-        if(mat[0][0]==0)return ans;
-        int x=0,y=0;
-        int n=mat.size();
         string path;
-        vector<vector<int>> visited(n,vector<int>(n,0));
-        solve(mat,ans,path,x,y,visited,n);
+        int n=mat.size();
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        solve(mat,visited,ans,path,srcx,srcy,n);
         return ans;
     }
 };
