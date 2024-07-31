fe26bebc62edef6707f90ca4806afdf081f86081
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-31 12:35:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Sorted Link List to BST/sorted-link-list-to-bst.cpp
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct LNode
+{
+    int data;
+    struct LNode* next;
+    
+    LNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TNode  
+{  
+    
+    int data;  
+    struct TNode* left;  
+    struct TNode* right; 
+    TNode(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+}; 
+
+void preOrder(TNode* node)  
+{  
+    if (node == NULL)  
+        return;  
+    cout<<node->data<<" ";  
+    preOrder(node->left);  
+    preOrder(node->right);  
+} 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+//User function Template for C++
+
+/* 
+//Linked List
+struct LNode
+{
+    int data;
+    struct LNode* next;
+    
+    LNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+//Tree
+struct TNode  
+{  
+    
+    int data;  
+    struct TNode* left;  
+    struct TNode* right; 
+    TNode(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+}; */
+class Solution{
+  public:
+  TNode*solve(vector<int>&ans,int s,int e){
+    if(s>e){
+        return NULL;
+    }
+    int mid=(s+e+1)/2;
+    TNode*root=new TNode(ans[mid]);
+    root->left=solve(ans,s,mid-1);
+    root->right=solve(ans,mid+1,e);
+    return root;
+}
+
+    TNode* sortedListToBST(LNode *head) {
+        //code here
+          if(head==NULL)return NULL;
+    vector<int>ans;
+    LNode*temp=head;
+    while(temp!=NULL){
+        ans.push_back(temp->data);
+        temp=temp->next;
+    }
+    return solve(ans,0,ans.size()-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        LNode *head = new LNode(data);
+        LNode *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new LNode(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        TNode* Thead = ob.sortedListToBST(head);
+        preOrder(Thead);
+        cout<<"\n";
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
