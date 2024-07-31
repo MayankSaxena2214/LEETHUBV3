d5308412378c90fcf56a00cbf258cb2bab7927b3
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-07-31 13:09:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for subsequence/check-for-subsequence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isSubSequence(string A, string B) 
+    {
+        // code here
+        int i=0;
+        for(int j=0;j<B.length();j++){
+            if(A[i]==B[j]){
+                i++;
+            }
+            if(i==A.length())return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;  
+        Solution ob;
+        if(ob.isSubSequence(A,B))
+            cout<<"1"<<endl;
+        else
+            cout<<"0"<<endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
