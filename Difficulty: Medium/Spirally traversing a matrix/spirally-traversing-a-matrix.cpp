c741aac158f68d7d8816d6613b338dc034f234af
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-01 13:15:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -6,32 +6,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to return a list of integers denoting spiral traversal of matrix.
-    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) {
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
         // code here
-       
-        int rowst=0,rowend=r-1,colst=0,colend=c-1;
-        int total=r*c;
-        int count=0;
         vector<int>ans;
-        while(count<total){
-            for(int col=colst;col<=colend&& count < total;col++){
-                ans.push_back(matrix[rowst][col]);
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int rowst=0,rowend=row-1;
+        int colst=0,colend=col-1;
+        int total=row*col;
+        int count=0;
+        while(count!=total){
+            for(int i=colst;i<=colend&&count<total;i++){
+                ans.push_back(matrix[rowst][i]);
                 count++;
             }
             rowst++;
-            for(int row=rowst;row<=rowend&& count < total;row++){
-                ans.push_back(matrix[row][colend]);
+            for(int i=rowst;i<=rowend && count<total;i++){
+                ans.push_back(matrix[i][colend]);
                 count++;
             }
             colend--;
-            for(int col=colend;col>=colst&& count < total;col--){
-                ans.push_back(matrix[rowend][col]);
+            for(int i=colend;i>=colst && count<total;i--){
+                ans.push_back(matrix[rowend][i]);
                 count++;
             }
             rowend--;
-            for(int row=rowend;row>=rowst&& count < total;row--){
-                ans.push_back(matrix[row][colst]);
+            for(int i=rowend;i>=rowst && count<total;i--){
+                ans.push_back(matrix[i][colst]);
                 count++;
             }
             colst++;
@@ -48,17 +49,16 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int> > matrix(r);
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
 
         for (int i = 0; i < r; i++) {
-            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
         }
 
         Solution ob;
-        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        vector<int> result = ob.spirallyTraverse(matrix);
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
