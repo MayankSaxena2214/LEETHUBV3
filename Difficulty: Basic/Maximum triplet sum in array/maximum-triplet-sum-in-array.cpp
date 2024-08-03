02c9b27de9140e8c650526ca59960457928b0cf8
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-03 17:38:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum triplet sum in array/maximum-triplet-sum-in-array.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int maxTripletSum(int arr[], int n)
+    {
+        if(n<3){
+            return -1;
+        }
+        sort(arr,arr+n);
+        return arr[n-1]+arr[n-2]+arr[n-3];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,i;
+	    cin>>n; int a[n];
+	    for(i=0;i<n;i++)
+	    cin>>a[i];
+	    Solution ob;
+	    cout <<ob.maxTripletSum(a, n);
+	    cout<<"\n";
+	}
+return 0;
+}
+// } Driver Code Ends
