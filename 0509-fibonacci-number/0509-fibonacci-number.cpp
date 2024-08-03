a2380995f47f2b517655349079960b0fe44e8884
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-03 16:43:08 +0530

    Time: 3 ms (66.73%), Space: 8.4 MB (5.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int fib(int n) {
-        int f=0;
-        int s=1;
-        int count=1,t;
-        for(int i=0;i<n;i++){
-            t=f+s;
-            count++;
-            if(count==n)return t;
-            f=s;
-            s=t;
+    vector<vector<int>> multiply(vector<vector<int>>a,vector<vector<int>>b){
+        vector<vector<int>>v={{0,0},{0,0}};
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<2;k++){
+                    v[i][j]+=a[i][k]*b[k][j];
+                }
+            }
         }
-        return n;
+        return v;
+    }
+    vector<vector<int>> fnc(vector<vector<int>>&a,int n){
+        
+        if(n==1)return a;
+        vector<vector<int>>temp=fnc(a,n/2);
+        if(n%2==0){
+            return multiply(temp,temp);
+        }
+        else{
+            return multiply(temp,multiply(temp,a));
+        }
+    }
+    int fib(int n) {
+        if(n==0)return 0;
+        vector<vector<int>>a={{1,1},{1,0}};
+        vector<vector<int>>ans=fnc(a,n);
+        return ans[0][1];
     }
 };
