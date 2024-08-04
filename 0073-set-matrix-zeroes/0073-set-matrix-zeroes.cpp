cbf6f8bb539ed5382eb18fead4da2adfaaf3feec
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-04 13:21:43 +0530

    Time: 16 ms (11.44%), Space: 16.8 MB (30.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,50 +1,51 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //set markers for 0 row and 0 column
+        //check row 0 and column 0 contain zero or not
         int x=0,y=0;
         int row=matrix.size();
         int col=matrix[0].size();
         for(int i=0;i<col;i++){
             if(matrix[0][i]==0)x=1;
         }
+        
         for(int i=0;i<row;i++){
             if(matrix[i][0]==0)y=1;
         }
-        //set markers in the first row for the remaining matrix
+        //mark the zero
         for(int i=1;i<row;i++){
             for(int j=1;j<col;j++){
                 if(matrix[i][j]==0){
-                    matrix[0][j]=0;
                     matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        //proceassing from first row
-        for(int j=1;j<col;j++){
-            if(matrix[0][j]==0){
-                for(int i=0;i<row;i++){
-                    matrix[i][j]=0;
+        //make zeroes for marked
+        for(int i=1;i<col;i++){
+            if(matrix[0][i]==0){
+                for(int j=1;j<row;j++){
+                    matrix[j][i]=0;
                 }
             }
         }
         for(int i=1;i<row;i++){
             if(matrix[i][0]==0){
-                for(int j=0;j<col;j++){
+                for(int j=1;j<col;j++){
                     matrix[i][j]=0;
                 }
             }
         }
-        if(x){
+        //process row 0 and col0
+        if(x==1){
             for(int j=0;j<col;j++){
                 matrix[0][j]=0;
             }
         }
-        if(y){
+        if(y==1){
             for(int i=0;i<row;i++){
                 matrix[i][0]=0;
             }
         }
-        
     }
 };
