751eeff5dee98bd787a4bca9d3803c21bf908503
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-08 19:00:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void solve(string&digits,vector<string>&ans,string output,auto&mp,int index){
-        if(index>=digits.length()){
-            ans.push_back(output);
-            return;
+unordered_map<int,string>mp={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
+
+    void solve(string &digits,auto&ans,string temp,int index){
+        if(index==digits.size()){
+            ans.push_back(temp);
+            return ;
         }
-        char digit=digits[index];
-        string mapped=mp[digit-'0'+0];
-        for(int i=0;i<mapped.length();i++){
-            output.push_back(mapped[i]);
-            solve(digits,ans,output,mp,index+1);
-            output.pop_back();
+        int idx=digits[index]-'0';
+        string word=mp[idx];
+        for(int i=0;i<word.length();i++){
+            temp.push_back(word[i]);
+            solve(digits,ans,temp,index+1);
+            temp.pop_back();
         }
+
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<int,string>mp={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
-        vector<string> ans;
-        if(digits.length()==0)return ans;
-        string output;
-        int index=0;
-        solve(digits,ans,output,mp,index);
+                if(digits.length()==0)return {};
+        vector<string>ans;
+        string temp;
+        solve(digits,ans,temp,0);
         return ans;
     }
 };
