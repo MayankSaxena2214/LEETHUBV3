72aaf52bc757e131f95e32b781d7af46687d442a
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-08 17:29:34 +0530

    Time: 3 ms (93.97%), Space: 8.8 MB (43.57%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     bool checkEqual(auto&count1,auto&count2){
         for(int i=0;i<26;i++){
-            if(count1[i]!=count2[i])return false;
+            if(count1[i]!=count2[i]){
+                return false;
+            }
         }
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        if(s1.length()>s2.length())return false;
         vector<int>count1(26,0);
         vector<int>count2(26,0);
+        if(s1.length()>s2.length())return false;
         for(int i=0;i<s1.length();i++){
             count1[s1[i]-'a']++;
             count2[s2[i]-'a']++;
         }
-        if(checkEqual(count1,count2))return true;
+        if(checkEqual(count1,count2)){
+            return true;
+        }
         int i=s1.length();
         while(i<s2.length()){
-            //removal logic
+            //old index remove
             int oldIndex=i-s1.length();
             count2[s2[oldIndex]-'a']--;
-            //addition  logic
+            //new index
             count2[s2[i]-'a']++;
-            if(checkEqual(count1,count2)){
-                return true;
-            }
+            if(checkEqual(count1,count2))return true;
             i++;
         }
         return false;
