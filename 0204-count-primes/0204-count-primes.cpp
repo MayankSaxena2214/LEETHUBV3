68b5b759698d78a8c24dbf260420b5c82212f9f9
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-08 18:10:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        vector<bool>prime(n+1,true);
+        if(n<=2)return 0;
+        prime[0]=false;
+        prime[1]=false;
         int count=0;
-        vector<bool>prime(n,true);
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
             if(prime[i]){
                 count++;
-                for(int j=2*i;j<n;j=j+i){
+                for(int j=2*i;j<=n;j+=i){
                     prime[j]=false;
                 }
             }
         }
         return count;
+        
     }
 };
