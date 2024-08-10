33b6834b9c6826316d796c639151d7d7a0780de8
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-10 21:08:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -42,13 +42,12 @@ class Solution
     public:
     //Function to return list containing elements of right view of binary tree.
     void solve(Node*root,int level,vector<int>&ans){
-    if(root==NULL)return;
-    if(level==ans.size()){
-        ans.push_back(root->data);
+    if(root==NULL){
+        return ;
     }
+    if(level==ans.size())ans.push_back(root->data);
     solve(root->right,level+1,ans);
     solve(root->left,level+1,ans);
-    
 }
     vector<int> rightView(Node *root)
     {
