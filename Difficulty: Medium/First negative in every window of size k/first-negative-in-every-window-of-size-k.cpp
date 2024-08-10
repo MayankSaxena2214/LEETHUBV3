9fb0e30758baaedca64ed8c2e823690aa54ec352
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-10 16:55:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -29,26 +29,22 @@ int main() {
 // } Driver Code Ends
 
 
-vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) {
-               
-    deque<long long>dq;
-    vector<long long> ans;
-    for(int i=0;i<K;i++){
-        if(A[i]<0)dq.push_back(i);
-        
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int k) {
+    
+    queue<int>q;
+    vector<long long > ans;
+    for(int i=0;i<k;i++){
+        if(A[i]<0)q.push(i);
     }
-    if(dq.size()>0)ans.push_back(A[dq.front()]);
-    else{
-        ans.push_back(0);
-    }
-    for(int i=K;i<N;i++){
-        //removal logic
-        if(!dq.empty() && i-dq.front()>=K)dq.pop_front();
-        //addition logic
-        if(A[i]<0)dq.push_back(i);
-        
-        if(dq.size()>0)ans.push_back(A[dq.front()]);
+    if(q.size()>0)ans.push_back(A[q.front()]);
+    else ans.push_back(0);
+    for(int i=k;i<N;i++){
+        if(!q.empty() && i-q.front()>=k){
+            q.pop();
+        }
+        if(A[i]<0)q.push(i);
+        if(q.size()>0)ans.push_back(A[q.front()]);
         else ans.push_back(0);
     }
     return ans;
- }
+}
