a56995cfb3c986be7fc1f3959954d90811916ffd
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-11 15:42:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -10,19 +10,23 @@ class Solution
     //Function to return the minimum cost of connecting the ropes.
     long long minCost(long long arr[], long long n) {
         // Your code here
-        long long ans=0;
-        priority_queue<long long ,vector<long long>,greater<long long>>pq;
-        for(int i=0;i<n;i++){
-            pq.push(arr[i]);
+        priority_queue<long long,vector<long long>,greater<long long>>pq;
+        for(long long i=0;i<n;i++){
+            long long val=arr[i];
+            pq.push(val);
+            
         }
+        long long ans=0;
         while(pq.size()>1){
             long long a=pq.top();
             pq.pop();
-            long long  b=pq.top();
+            long long b=pq.top();
             pq.pop();
-            long long sum=a+b;
-            pq.push(sum);
+            long long sum=0;
+            sum+=a;
+            sum+=b;
             ans+=sum;
+            pq.push(sum);
         }
         return ans;
     }
