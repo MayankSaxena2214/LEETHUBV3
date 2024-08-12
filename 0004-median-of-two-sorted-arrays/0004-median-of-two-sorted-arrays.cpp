870ca8853dc6961557029b9d242d7c0aedac1630
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-12 15:27:27 +0530

    Time: 20 ms (79%), Space: 94.8 MB (47.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(auto i:nums1)ans.push_back(i);
-        for(auto i:nums2)ans.push_back(i);
-        sort(ans.begin(),ans.end());
-        int n=ans.size();
-        if(n%2!=0){
-            return ans[ans.size()/2]*1.0;
+        int n=nums1.size();
+        int m=nums2.size();
+        if(n>m)return findMedianSortedArrays(nums2,nums1);
+
+        int l=0;
+        int r=n;
+        while(l<=r){
+            int Px=(l+(r-l)/2);
+            int Py=(m+n+1)/2-Px;
+
+            //left vaale element
+            int x1=(Px==0)?INT_MIN:nums1[Px-1];
+            int y1=(Py==0)?INT_MIN:nums2[Py-1];
+            //right vaale
+            int x2=(Px==n)?INT_MAX:nums1[Px];
+            int y2=(Py==m)?INT_MAX:nums2[Py];
+
+            if(x1<=y2 && y1<=x2){
+                if((m+n)%2==0){
+                    return (max(x1,y1)+min(x2,y2))/2.0;
+                }
+                else{
+                    return max(x1,y1);
+                }
+            }
+            if(x1>y2){
+                r=Px-1;
+            }
+            else{
+                l=Px+1;
+            }
         }
-        else{
-            int val1=ans[n/2];
-            int val2=ans[n/2-1];
-            return ((val1+val2)/2.0);
-        }
-        
+        return -1;
     }
 };
