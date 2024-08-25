d03d7a8586e1f7808b37b8182b71ed3d7e7e2794
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-25 12:32:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.cpp
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -4,42 +4,45 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function is to check whether two strings are anagram of each other or not.
-    bool isAnagram(string a, string b){
-        
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string s, string t) {
+
         // Your code here
-        vector<int>v1(26,0);
-        vector<int>v2(26,0);
-        for(auto val:a)v1[val-'a']++;
-        for(auto val:b)v2[val-'a']++;
-        for(int i=0;i<26;i++){
-            if(v1[i]!=v2[i])return false;
+        vector<int>v(26,0);
+        for(auto val:s){
+            v[val-'a']++;
+            
+        }
+        for(auto val:t){
+            v[val-'a']--;
+        }
+        for(auto val:v){
+            if(val!=0)return false;
         }
         return true;
     }
-
 };
 
 //{ Driver Code Starts.
 
 int main() {
-    
+
     int t;
 
     cin >> t;
 
-    while(t--){
+    while (t--) {
         string c, d;
 
         cin >> c >> d;
         Solution obj;
-        if(obj.isAnagram(c, d)) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        if (obj.isAnagram(c, d))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
-
 }
 
 // } Driver Code Ends
