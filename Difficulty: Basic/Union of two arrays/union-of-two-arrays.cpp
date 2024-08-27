d472c8126d9f5d965be16a2c036b8ec1c465cdf7
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-27 23:19:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Union of two arrays/union-of-two-arrays.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+    int doUnion(vector<int> arr1, vector<int> arr2) {
+        // code here
+        unordered_set<int>st(arr1.begin(),arr1.end());
+        int count=st.size();
+        for(auto val:arr2){
+            if(st.find(val)==st.end()){
+                count++;
+                st.insert(val);
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> a;
+        vector<int> b;
+
+        string input;
+        // For a
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // For b
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.doUnion(a, b) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
