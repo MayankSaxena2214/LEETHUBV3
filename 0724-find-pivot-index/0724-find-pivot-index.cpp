d7b4565f266e17654f009706b2bfe91d98c034ad
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-28 23:01:57 +0530

    Time: 17 ms (55.83%), Space: 35.2 MB (22.46%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int>sumLeft,sumRight(n);
+        vector<int>suffix(n);
         int sum=0;
-        for(auto val:nums){
-            sum+=val;
-            sumLeft.push_back(sum);
+        for(int i=n-1;i>=0;i--){
+            suffix[i]=sum;
+            sum+=nums[i];
         }
         sum=0;
-        for(int i=n-1;i>=0;i--){
-            sum+=nums[i];
-            sumRight[i]=sum;
-        }
         for(int i=0;i<n;i++){
-            if(sumLeft[i]==sumRight[i]){
+            if(sum==suffix[i]){
                 return i;
             }
+            sum+=nums[i];
         }
         return -1;
-
     }
 };
