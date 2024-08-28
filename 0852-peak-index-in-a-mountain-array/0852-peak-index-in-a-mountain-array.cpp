5c93c5266d4353265e0fad92b7ff6a097aab5676
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-28 22:58:05 +0530

    Time: 90 ms (9.97%), Space: 62.4 MB (41.97%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,15 +3,14 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n=arr.size();
         int s=0,e=n-1;
-        int mid=(s+e)/2;
         while(s<e){
-            if(arr[mid]<arr[mid+1]){
+            int mid=(s+(e-s)/2);
+            if(arr[mid]<=arr[mid+1]){
                 s=mid+1;
             }
             else{
                 e=mid;
             }
-            mid=(s+e)/2;
         }
         return s;
     }
