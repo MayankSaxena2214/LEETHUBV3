cfc5c6358db5c7f32852a9e4f4775968f4693f2d
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-08-28 22:45:10 +0530

    Time: 40 ms (61.8%), Space: 36.1 MB (86.29%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>ans;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
             int index=abs(nums[i])-1;
-            if(nums[index]<0)ans.push_back(abs(nums[i]));
-            else nums[index]=-nums[index];
+            if(nums[index]<0){
+                ans.push_back(abs(nums[i]));
+            }
+            else{
+                nums[index]=-nums[index];
+            }
         }
         return ans;
     }
