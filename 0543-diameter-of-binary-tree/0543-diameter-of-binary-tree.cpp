1fb88e3dab989fb1d31e769e7ef96c8b478088e9
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-01 14:09:09 +0530

    Time: 11 ms (71.45%), Space: 22.5 MB (51.22%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,23 +11,21 @@
  */
 class Solution {
 public:
-    pair<int,int> solve(TreeNode*root){
-       if(root==NULL){
-           return {0,0};
-       }
-        pair<int,int> left=solve(root->left);
-        pair<int,int> right=solve(root->right);
-        pair<int,int> ans;
+    pair<int,int> solve(auto&root){
+        if(root==NULL){
+            return {0,0};
+        }
+        auto left=solve(root->left);
+        auto right=solve(root->right);
+        pair<int,int>curr;
         int op1=left.first;
         int op2=right.first;
         int op3=left.second+right.second;
-        ans.first=max(op1,max(op2,op3));
-        ans.second=max(left.second,right.second)+1;
-        return ans;
+        curr.first=max(op1,max(op2,op3));
+        curr.second=max(left.second,right.second)+1;
+        return curr;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        pair<int,int> ans=solve(root);
-        return ans.first;
-        
+        return solve(root).first;
     }
 };
