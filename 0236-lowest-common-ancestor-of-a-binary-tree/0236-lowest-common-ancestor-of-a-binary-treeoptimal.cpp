5fb467d73f7553b1f763db42a2d3e68cc8fda533
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-01 18:08:31 +0530

    Update 0236-lowest-common-ancestor-of-a-binary-treeoptimal.cpp

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-treeoptimal.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-treeoptimal.cpp
@@ -9,29 +9,17 @@
  */
 class Solution {
 public:
-    bool findPath(auto root,auto &path,int val){
-        if(root==NULL)return false;
-
-        path.push_back(root);
-        if(root->val==val)return true;
-        if(findPath(root->left,path,val) || findPath(root->right,path,val)){
-            return true;
-        }
-
-        path.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>path1,path2;
-        if(!findPath(root,path1,p->val) || !findPath(root,path2,q->val)){
-            return NULL;
+        if(root==NULL)return NULL;
+        if(root->val==p->val || root->val==q->val){
+            return root;
         }
-        TreeNode*ans=NULL;
-        for(int i=0;i<path1.size() && i<path2.size();i++){
-            if(path1[i]->val==path2[i]->val){
-                ans=path1[i];
-            }
-        }
-        return ans;
+        auto left=lowestCommonAncestor(root->left,p,q);
+        auto right=lowestCommonAncestor(root->right,p,q);
+        if(left!=NULL && right!=NULL)return root;
+        else if(left==NULL && right!=NULL)return right;
+        else if(left!=NULL && right==NULL)return left;
+        return NULL;
+        
     }
 };
