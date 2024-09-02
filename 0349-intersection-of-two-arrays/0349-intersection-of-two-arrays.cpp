28807386df78208aaa6532285feca8894bf825ac
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-02 21:38:02 +0530

    Time: 4 ms (67.02%), Space: 13.1 MB (64.68%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,18 +4,13 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         int i=0,j=0;
-        vector<int>ans;
+        set<int>st;
         int size1=nums1.size(),size2=nums2.size();
         while(i<size1 && j<size2){
             if(nums1[i]==nums2[j]){
-                ans.push_back(nums1[i]);
-                int curr=nums1[i];
-                while(i<size1 && nums1[i]==curr){
-                    i++;
-                }
-                while(j<size2 && nums2[j]==curr){
-                    j++;
-                }
+                st.insert(nums1[i]);
+                i++;
+                j++;
             }
             else if(nums1[i]<nums2[j]){
                 i++;
@@ -24,6 +19,12 @@ public:
                 j++;
             }
         }
+        //put element in vector
+        vector<int>ans;
+        for(auto val:st){
+            ans.push_back(val);
+        }
         return ans;
+        
     }
 };
