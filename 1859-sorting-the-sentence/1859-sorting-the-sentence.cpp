2bbf867c1a0dea39a34cb219454393ff6e37b74b
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-02 22:54:30 +0530

    Time: 3 ms (43.75%), Space: 7.9 MB (24.15%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     string sortSentence(string s) {
         vector<string>v(10);
+        string ans;
         string curr;
         for(auto ch:s){
             if(ch==' '){
                 continue;
             }
-            else if(ch>='0' && ch<='9'){
-                int index=ch-'0';
-                v[index]+=curr+" ";
+            if(ch>='0' && ch<='9'){
+                v[ch-'0']=curr;
                 curr="";
             }
             else{
                 curr+=ch;
             }
         }
-        string ans;
         for(auto str:v){
+            if(str.empty())continue;
             ans+=str;
+            ans+=" ";
         }
         if(ans.length()>0){
             ans.pop_back();
-            return ans;
         }
         return ans;
     }
