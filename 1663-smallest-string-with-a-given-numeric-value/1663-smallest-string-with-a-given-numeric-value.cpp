404a8570b9f62ce088bd8aa7dc97bcf3800f2dcc
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-05 18:18:10 +0530

    Time: 50 ms (49.86%), Space: 46.3 MB (5.63%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        if(n>k){
-            return "";
-        }
+        if(k<n)return "";
         vector<int>v(n,1);
         k=k-n;
         int i=n-1;
-        while(k){
-            if(k>25){
+        string ans;
+        while(k>0){
+            if(k>=25){
                 v[i]=v[i]+25;
-                k=k-25;
                 i--;
+                k=k-25;
             }
             else{
                 v[i]=v[i]+k;
@@ -19,10 +18,11 @@ public:
                 i--;
             }
         }
-        string ans;
         for(auto val:v){
-            ans.push_back(val-1+'a');
+            char ch=val-1+'a';
+            ans+=ch;
         }
         return ans;
+
     }
 };
