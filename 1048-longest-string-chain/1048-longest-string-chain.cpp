07b8ce709736d80e3e1ca3d75557883b566e05b4
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-05 19:02:28 +0530

    Time: 353 ms (28.82%), Space: 64.4 MB (34.99%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-static bool sortByLength(string a, string b){
-    return a.length() < b.length();
-}
+    static bool custom(string a,string b){
+        return a.length()<b.length();
+    }
     int longestStrChain(vector<string>& words) {
-        int ans=INT_MIN;
+        sort(words.begin(),words.end(),custom);
         map<string,int>dp;
-        sort(words.begin(),words.end(),sortByLength);
-        for(int i=0;i<words.size();i++){
-            string word=words[i];
-            for(int j=0;j<word.length();j++){
-                string pred=word.substr(0,j)+word.substr(j+1);
+        int ans=0;
+        for(auto word:words){
+
+            for(int i=0;i<word.length();i++){
+                string pred=word.substr(0,i)+word.substr(i+1);
                 dp[word]=max(dp[word],dp[pred]+1);
             }
-            
-                ans=max(ans,dp[word]);
-            
+            ans=max(ans,dp[word]);
         }
         return ans;
     }
