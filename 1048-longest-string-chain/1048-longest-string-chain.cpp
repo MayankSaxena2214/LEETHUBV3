0c101a23832695d21b96902a46ebeda15b3447c3
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 23:19:53 +0530

    Time: 345 ms (28.53%), Space: 64.3 MB (34.79%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,17 +4,16 @@ public:
         return a.length()<b.length();
     }
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),custom);
         map<string,int>dp;
         int ans=0;
+        sort(words.begin(),words.end(),custom);
         for(auto word:words){
-
             for(int i=0;i<word.length();i++){
                 string pred=word.substr(0,i)+word.substr(i+1);
                 dp[word]=max(dp[word],dp[pred]+1);
             }
             ans=max(ans,dp[word]);
         }
-        return ans;
+    return ans;
     }
 };
