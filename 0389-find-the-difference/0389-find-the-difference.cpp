903bbe81fbf74819548269cfaf008baedec8d656
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 22:25:55 +0530

    Time: 4 ms (50.55%), Space: 8.1 MB (85.3%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ans=0;
+        char ans=NULL;
         for(auto ch:s)ans^=ch;
         for(auto ch:t)ans^=ch;
         return ans;
