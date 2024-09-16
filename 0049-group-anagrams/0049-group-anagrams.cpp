9ec6f1510b6308a5e7caf2ce249a7609ede8fc5f
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 23:48:54 +0530

    Time: 111 ms (5.01%), Space: 28.9 MB (15.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+    string generate(string str){
+        string ans;
+        vector<int>v(26,0);
+        for(auto ch:str){
+            v[ch-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            while(v[i]>0){
+                char ch=i+'a';
+                cout<<ch<<"Index is "<<i<<endl;
+                ans+=ch;
+                v[i]--;
+            }
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         map<string,vector<string>>mp;
         for(auto str:strs){
-            string sorted=str;
-            sort(sorted.begin(),sorted.end());
+            string sorted=generate(str);
+            cout<<sorted<<endl;
             mp[sorted].push_back(str);
 
         }
