da4ff7245e1f723da98707b1267bbdfd4f094f65
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 23:56:44 +0530

    Time: 0 ms (100%), Space: 8.8 MB (55.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector<int>v(26,0);
+        for(auto ch:s){
+            v[ch-'a']++;
+
+        }
+        for(auto ch:t){
+            v[ch-'a']--;
+        }
+        for(auto val:v){
+            if(val!=0)return false;
+        }
+        return true;
+
     }
 };
