0a472c34f8caec895fb84e8292c377e0066d1781
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 23:03:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.length()==0)return true;
-        int i=0;
-        for(int j=0;j<t.length();j++){
-            if(s[i]==t[j]){
-                i++;
-                if(i==s.length())return true;
+        int j=0;
+        for(int i=0;i<t.length();i++){
+            if(s[j]==t[i]){
+                j++;
+                if(j==s.length())return true;
             }
-            
         }
         return false;
     }
