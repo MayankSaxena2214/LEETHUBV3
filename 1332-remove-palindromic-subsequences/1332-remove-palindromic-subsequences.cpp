8036eb1eed9ec0e45853be5bcb18753871f49f19
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 21:49:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (60.69%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int removePalindromeSub(string s) {
         if(s.length()==0)return 0;
-        int i=0,j=s.length()-1;
-        while(i<j){
-            if(s[i++]!=s[j--]){
-                return 2;
-            }
-        }
-        return 1;
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        if(s==rev)return 1;
+        return 2;
     }
 };
