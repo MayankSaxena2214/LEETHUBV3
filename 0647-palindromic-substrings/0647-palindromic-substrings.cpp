c638dc33e48fef6075d1aacdaab9b995f5801441
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 21:43:55 +0530

    Time: 21 ms (43.83%), Space: 33.1 MB (19.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,10 +3,9 @@ public:
     int countSubstrings(string s) {
         int count=0;
         int n=s.length();
-        
         vector<vector<int>>dp(n,vector<int>(n,0));
         for(int diff=0;diff<n;diff++){
-            for(int i=0,j=diff;i<n, j<n;i++,j++){
+            for(int i=0,j=diff;j<n;j++,i++){
                 if(diff==0){
                     dp[i][j]=1;
                 }
@@ -26,7 +25,7 @@ public:
                         dp[i][j]=0;
                     }
                 }
-                if(dp[i][j]!=0){
+                if(dp[i][j]){
                     count++;
                 }
             }
