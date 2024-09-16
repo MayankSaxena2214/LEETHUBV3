e80bb26f8bffc4005a22f7041516465105b49fa5
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 23:46:18 +0530

    Time: 3 ms (77.36%), Space: 8.8 MB (44.01%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
+        int n=num1.length(),m=num2.length();
         if(num1=="0" || num2=="0")return "0";
-        int n=num1.size();
-        int m=num2.size();
-        vector<int>num(n+m,0);
+        vector<int>nums(n+m,0);
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                int val=num[i+j+1]+(num1[i]-'0')*(num2[j]-'0');
-                num[i+j+1]=val%10;
-                num[i+j]+=val/10;
+                nums[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+                nums[i+j]+=nums[i+j+1]/10;
+                nums[i+j+1]%=10;
             }
         }
-        string result;
-        for(int i=0;i<num.size();i++){
-            result+=to_string(num[i]);
+        string ans;
+        for(int i=0;i<n+m;i++){
+            ans+=nums[i]+'0';
         }
-        cout<<result<<endl;
-        //leading zero removal
-        int i;
-        for( i=0;i<result.size() && result[i]=='0';i++){
+        int i=0;
+        while(i<n+m && ans[i]=='0'){
+            i++;
+        }
+        return ans.substr(i);
 
-        }
-        return result.substr(i);      
+
     }
 };
