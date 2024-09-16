c534ed994876430610933008a14904424d406001
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-16 22:22:43 +0530

    Time: 0 ms (100%), Space: 7.7 MB (20.73%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
+        string curr;
         int count=0;
-        string check;
         int n=searchWord.length();
         for(auto ch:sentence){
             if(ch==' '){
-                if(check.length()>=n && check.substr(0,n)==searchWord){
+                if(curr.length()>=n && curr.substr(0,n)==searchWord){
                     return count+1;
                 }
                 else{
+                    curr="";
                     count++;
-                    check="";
                 }
             }
             else{
-                check+=ch;
+                curr+=ch;
             }
         }
-        if(check.length()>=n && check.substr(0,n)==searchWord)return count+1;
+        if(curr.length()>=n && curr.substr(0,n)==searchWord){
+            return count+1;
+        }
         return -1;
     }
 };
