42c72ad0a637832cf17b8c0322d5fe04e559daa2
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 15:10:34 +0530

    Time: 122 ms (18.81%), Space: 22 MB (57.52%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,12 +1,16 @@
 class NumArray {
 public:
-vector<int>nums;
+    vector<int>nums;
     NumArray(vector<int>& nums) {
         this->nums=nums;
     }
     
     int sumRange(int left, int right) {
-        return accumulate(nums.begin()+left,nums.begin()+right+1,0);
+        int sum=0;
+        for(int i=left;i<=right;i++){
+            sum+=nums[i];
+        }
+        return sum;
     }
 };
 
