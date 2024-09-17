e952763c7db9526ea0229d91de9daa9c9f017997
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 18:35:31 +0530

    Time: 17 ms (9.49%), Space: 14.7 MB (81.9%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        int med=nums[n/2];
+        sort(nums.begin(),nums.end());
+
+        int avg=nums[n/2];
         int ans=0;
         for(auto val:nums){
-            ans+=abs(med-val);
+            cout<<abs(val-avg)<<endl;
+            ans+=abs(val-avg);
         }
         return ans;
     }
