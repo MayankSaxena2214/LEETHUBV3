beda23967f0a2fe507e8a81107f37a6565cdc785
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 20:21:37 +0530

    Time: 7 ms (40.77%), Space: 19.8 MB (16.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,43 +10,33 @@
  */
 class Solution {
 public:
-    ListNode*solve(ListNode*first,ListNode*second){
-    if(first->next==NULL){
-        first->next=second;
-        return first;
-    }
-    ListNode*curr1=first,*next1=curr1->next,*curr2=second,*next2=NULL;
-    while(curr2!=NULL && next1!=NULL){
-        if(curr2->val>=curr1->val && curr2->val<=next1->val){
-            //we can insert the curr2 inside the curr1 and next1
-            curr1->next=curr2;
-            next2=curr2->next;
-            curr2->next=next1;
-            curr1=curr2;
-            curr2=next2; 
-        }
-        else{
-            //curr1 and next1 ko aage badhao
-            curr1=next1;
-            next1=next1->next;
-            if(next1==NULL){
-                curr1->next=curr2;
-                return first;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode*first=list1;
+        ListNode*second=list2;
+        ListNode*anshead=new ListNode(0);
+        ListNode*anstail=anshead;
+        while(first && second){
+            if(first->val<second->val){
+                anstail->next=first;
+                anstail=first;
+                first=first->next;
+            }
+            else{
+                anstail->next=second;
+                anstail=second;
+                second = second->next;
             }
         }
-    }
-    return first;
-}
-    ListNode* mergeTwoLists(ListNode* first, ListNode* second) {
-         // Write your code here.
-    if(first==NULL)return second;
-    if(second==NULL)return first;
-   
-    if(first->val<second->val){
-        return solve(first,second);
-    }
-    else{
-        return solve(second,first);
-    }
+        while(first){
+            anstail->next=first;
+            anstail=first;
+            first=first->next;
+        }
+        while(second){
+            anstail->next=second;
+            anstail=second;
+            second = second->next;
+        }
+        return anshead->next;
     }
 };
