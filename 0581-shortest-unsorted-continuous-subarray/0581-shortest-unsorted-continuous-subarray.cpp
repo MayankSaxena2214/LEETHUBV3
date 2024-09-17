4b5749b536bf00f82c144ff15948ad8f5564fe5c
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 15:08:52 +0530

    Time: 21 ms (61%), Space: 29.5 MB (82.66%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-    int findUnsortedSubarray(vector<int>& arr) {
-        if(arr.size()==1)return 0;
-        int n=arr.size();
-        int maxi=INT_MIN,mini=INT_MAX;
-        bool flag=1;
+    int findUnsortedSubarray(vector<int>& nums) {
+        int mini=INT_MAX,maxi=INT_MIN;
+        int n=nums.size();
+        if(n<=1)return 0;
         for(int i=0;i<n;i++){
             if(i==0){
-                if(arr[i]>arr[i+1]){
-                    flag=0;
-                    maxi=max(maxi,arr[i]);
-                    mini=min(mini,arr[i]);
+                if(nums[i]>nums[i+1]){
+                    if(mini>nums[i]){
+                        mini=nums[i];
+                    }
+                    if(maxi<nums[i]){
+                        maxi=nums[i];
+                    }
                 }
             }
             else if(i==n-1){
-                if(arr[i]<arr[i-1]){
-                    flag=0;
-                    maxi=max(maxi,arr[i]);
-                    mini=min(mini,arr[i]);
+                if(nums[i]<nums[i-1]){
+                    if(mini>nums[i]){
+                        mini=nums[i];
+                    }
+                    if(maxi<nums[i]){
+                        maxi=nums[i];
+                    }
                 }
             }
             else{
-                if(arr[i]>arr[i+1] || arr[i]<arr[i-1]){
-                    flag=0;
-                    maxi=max(maxi,arr[i]);
-                    mini=min(mini,arr[i]);
+                if(nums[i]>nums[i+1] || nums[i]<nums[i-1]){
+                    if(mini>nums[i]){
+                        mini=nums[i];
+                    }
+                    if(maxi<nums[i]){
+                        maxi=nums[i];
+                    }
                 }
             }
         }
-        if(flag==1)return 0;
-        int i,j;
-        for(i=0;i<n && arr[i]<=mini;i++){
-            
+        if(mini==INT_MAX || maxi==INT_MIN){
+            return 0;
         }
-        for(j=n-1;j>=0 && arr[j]>=maxi;j--){}
+        int i;
+        for(i=0;i<n && nums[i]<=mini;i++){}
+        int j;
+        for(j=n-1;j>=0 && nums[j]>=maxi;j--){}
         return j-i+1;
+
     }
 };
