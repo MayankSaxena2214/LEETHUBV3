9ab0da2165480a5bb80e8a921cc4b32ebfaea9df
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 18:39:33 +0530

    Time: 4 ms (61.66%), Space: 7.7 MB (38.07%) - LeetHub

--- 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<int> memLeak(int memory1, int memory2) {
         int t=1;
-        while(1){
+        while(true){
             if(memory1>=t || memory2>=t){
                 if(memory1>=memory2){
                     memory1-=t;
+
                 }
                 else{
                     memory2-=t;
@@ -13,7 +14,11 @@ public:
                 t++;
             }
             else{
-                return {t,memory1,memory2};
+                vector<int>ans;
+                ans.push_back(t);
+                ans.push_back(memory1);
+                ans.push_back(memory2);
+                return ans;
             }
         }
         return {};
