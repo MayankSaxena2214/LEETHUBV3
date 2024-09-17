76b08b254d73dc8ae8fbbf7ca9621f21940775fb
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 19:00:07 +0530

    Time: 0 ms (100%), Space: 7.4 MB (74.56%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
         if(n==0)return 1;
-        if(n==1)return 10;
         int ans=10;
-        int start=9;
-        int temp=9;
-        while(n-1>0 && start){
-            temp=temp*start;
+        int start=9,current=9;
+        while(n>1 && start){
+            current*=start;
             start--;
-            ans+=temp;
             n--;
+            ans+=current;
         }
         return ans;
     }
