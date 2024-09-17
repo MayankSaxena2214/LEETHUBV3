663d7e77950ac367ead85b89182917a5cea79532
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 12:20:26 +0530

    Time: 12 ms (57.17%), Space: 24.8 MB (45.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        for(int i=0;i<n;i++){
-            if(i==0){
-                left[i]=arr[i];
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int leftbar=height[0];
+        int rightbar=height[n-1];
+        int left=0,right=n-1;
+        int ans=0;
+        while(left<=right){
+            if(leftbar<rightbar){
+                if(height[left]>leftbar){
+                    leftbar=height[left];
+                    // left++;
+                }
+                else{
+                    ans+=leftbar-height[left];
+                    left++;
+                }
             }
             else{
-                left[i]=max(left[i-1],arr[i]);
+                if(height[right]>rightbar){
+                    rightbar=height[right];
+                    // right--;
+                }
+                else{
+                    ans+=rightbar-height[right];
+                    right--;
+                }
             }
-        }
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                right[i]=arr[i];
-            }
-            else{
-                right[i]=max(right[i+1],arr[i]);
-            }
-        }
-        long long ans=0;
-        for(int i=1;i<n-1;i++){
-            ans+=min(left[i],right[i])-arr[i];
         }
         return ans;
     }
