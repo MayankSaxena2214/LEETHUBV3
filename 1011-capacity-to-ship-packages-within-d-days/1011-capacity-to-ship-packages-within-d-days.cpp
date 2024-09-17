c03af3685a58651cfcfe2a694d5f766bee70f6b5
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-09-17 22:53:30 +0530

    Time: 49 ms (6.83%), Space: 33.9 MB (10.78%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    bool isPossible(auto&weights,int mid,int days){
+    bool isPossible(vector<int>&weights,int days,int mid){
         int mydays=1;
         long long int sum=0;
-        for(auto val:weights){
-            if(val+sum<=mid){
-                sum+=val;
+        for(int i=0;i<weights.size();i++){
+            if(sum+weights[i]<=mid){
+                sum+=weights[i];
             }
             else{
+                sum=weights[i];
                 mydays++;
-                sum=val;
-                if(mydays>days)return false;
+
             }
         }
-        return true;
+        if(mydays<=days){
+            return true;
+        }
+        return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
         long long int low=*max_element(weights.begin(),weights.end());
         long long int high=accumulate(weights.begin(),weights.end(),0);
-        int ans=-1;
+        long long int ans=-1;
         while(low<=high){
-            long long int mid=low+(high-low)/2;
-            if(isPossible(weights,mid,days)){
+            long long int mid=(low+high)/2;
+            if(isPossible(weights,days,mid)){
                 ans=mid;
                 high=mid-1;
             }
