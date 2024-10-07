6cac4a5d90f7e38373ab6d7dc322dbf6a625189a
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-07 19:40:38 +0530

    Time: 61 ms (5.44%), Space: 8.9 MB (83.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         for(int i=0;i<s.length();i++){
-        if(s[i]>='a' && s[i]<='z'){
-            continue;
+            if(s[i]>='a' && s[i]<='z')continue;
+            else if(s[i]>='0' && s[i]<='9')continue;
+            else if(s[i]>='A' && s[i]<='Z'){
+                int jump=s[i]-'A';
+                s[i]=jump+'a';
+            }
+            else{
+                s.erase(i,1);
+                i--;
+            }
         }
-        else if(s[i]>='A' && s[i]<='Z'){
-            s[i]=tolower(s[i]);
+        for(auto ch:s)cout<<ch;
+        int low=0,high=s.length()-1;
+        while(low<high){
+            if(s[low]!=s[high]){
+                return false;
+            }
+            low++;
+            high--;
         }
-        else if(s[i]>='0' && s[i]<='9'){
-            continue;
-        }
-        else{
-            s.erase(i,1);
-            i--;
-        }
-    }
-    // cout<<s<<endl;
-    int start=0,end=s.length()-1;
-    while(start<end){
-        if(s[start]!=s[end]){
-            return false;
-        }
-        start++,end--;
-    }
-    return true;
+        return true;
     }
 };
