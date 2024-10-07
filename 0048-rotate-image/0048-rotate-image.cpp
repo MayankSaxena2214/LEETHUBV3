ff81c46a4373ee335168dbb73a914f41f5d6788d
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-07 21:56:35 +0530

    Time: 0 ms (100%), Space: 9 MB (10.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,13 +3,17 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int rows=matrix.size();
         int cols=matrix[0].size();
-        for(int i=0;i<rows;i++){
+
+        //transpose
+        for(int i=0;i<rows-1;i++){
             for(int j=i+1;j<cols;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        //reverse each row
         for(int i=0;i<rows;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
+        
     }
 };
