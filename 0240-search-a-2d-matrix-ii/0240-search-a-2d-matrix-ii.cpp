4c0a0bf27f633c0e98fd7aa3decff3a652366a66
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-07 22:20:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,17 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows=matrix.size();
         int cols=matrix[0].size();
+        int total=rows*cols;
+        int start=0,end=total-1;
         int i=0;
         int j=cols-1;
-        while(i<rows && j>=0){
-            int val=matrix[i][j];
-            if(val==target)return true;
-            else if(val>target){
-                j--;
-            }
-            else{
-                i++;
-            }
+        while(i<rows && cols>=0){
+            int value=matrix[i][j];
+            if(value==target)return true;
+            else if(value>target)j--;
+            else i++;
         }
         return false;
     }
