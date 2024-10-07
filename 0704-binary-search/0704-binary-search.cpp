2f5f1c844ad0b4e12c5303a3be601db06c0a81e6
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-07 16:51:33 +0530

    Time: 22 ms (86.51%), Space: 30 MB (88.5%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-int binarySearch(vector<int>arr,int s,int e,int k){
-   
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==k){
-            return mid;
-        }
-        else if(arr[mid]>k){
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-        mid=s+(e-s)/2;
-    }
-    return -1;
-}
     int search(vector<int>& nums, int target) {
-        return binarySearch(nums,0,nums.size()-1,target);
-        
+        int s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)e--;
+            else s++;
+        }
+        return -1;
     }
 };
