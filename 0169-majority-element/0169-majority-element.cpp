f25ff86f9a27e6b84e56e1dc160187354a94158d
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-16 23:42:18 +0530

    Time: 12 ms (89.12%), Space: 27 MB (49.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate=INT_MIN;
         int life=0;
-        for(auto val:nums){
-            if(candidate==val){
+        int candidate=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate){
                 life++;
             }
             else if(life==0){
-                candidate=val;
-                life=1;
+                candidate=nums[i];
+                life++;
             }
             else{
                 life--;
