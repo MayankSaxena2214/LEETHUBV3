587d5926e9a023a4f89e4a7cb023e91a76722d57
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-25 23:30:01 +0530

    Time: 56 ms (89.04%), Space: 62.3 MB (15.68%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,43 +12,38 @@
 class Solution {
 public:
     int deepestLeavesSum(TreeNode* root) {
-        if(root==NULL)return 0;
-        //find level 
         int level=0;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             int n=q.size();
             for(int i=0;i<n;i++){
-                TreeNode*front=q.front();
+                auto top=q.front();
                 q.pop();
-                if(front->left)q.push(front->left);
-                if(front->right)q.push(front->right);
-
+                if(top->left)q.push(top->left);
+                if(top->right)q.push(top->right);
             }
             level++;
         }
         int count=0;
-        queue<TreeNode*>q1;
-        q1.push(root);
-        while(!q1.empty()){
-            int n=q1.size();
-            if(count==level-1)break;
+        q.push(root);
+        while(count<level-1){
+            int n=q.size();
             for(int i=0;i<n;i++){
-                TreeNode*front=q1.front();
-                q1.pop();
-                if(front->left)q1.push(front->left);
-                if(front->right)q1.push(front->right);
-
+                auto top=q.front();
+                q.pop();
+                if(top->left)q.push(top->left);
+                if(top->right)q.push(top->right);
             }
             count++;
+        }
+        int ans=0;
+        while(!q.empty()){
+            ans+=q.front()->val;
+            q.pop();
+        }
+        return ans;
             
-        }
-        int sum=0;
-        while(!q1.empty()){
-            sum+=q1.front()->val;
-            q1.pop();
-        }
-        return sum;
+        
     }
 };
