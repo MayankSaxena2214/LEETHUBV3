5655a6f47d8b1844da05b961fed7bddac061da82
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-29 10:50:06 +0530

    Time: 0 ms (100%), Space: 12.8 MB (74.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int i=0,j=0;
+        vector<int>ans;
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        int i=0,j=0;
-        set<int>st;
-        int size1=nums1.size(),size2=nums2.size();
-        while(i<size1 && j<size2){
+        int n1=nums1.size();
+        int n2=nums2.size();
+        while(i<n1 && j<n2){
             if(nums1[i]==nums2[j]){
-                st.insert(nums1[i]);
-                i++;
-                j++;
+                int val=nums1[i];
+                ans.push_back(val);
+                while(i<n1 && nums1[i]==val){
+                    i++;
+                }
+                while(j<n2 && nums2[j]==val)j++;
             }
-            else if(nums1[i]<nums2[j]){
-                i++;
+            else if(nums1[i]>nums2[j]){
+                j++;
             }
             else{
-                j++;
+                i++;
             }
         }
-        //put element in vector
-        vector<int>ans;
-        for(auto val:st){
-            ans.push_back(val);
-        }
         return ans;
-        
     }
 };
