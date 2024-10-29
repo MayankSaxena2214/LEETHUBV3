d25ae8dd6649b58e733ffa51f6a2babded4ce122
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-29 15:18:25 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.24%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,24 +3,24 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int rows=matrix.size();
         int cols=matrix[0].size();
-        int rowst=0,rowend=rows-1;
-        int colst=0,colend=cols-1;
-        int count=0;
+        int rowst=0,colst=0;
+        int rowend=rows-1,colend=cols-1;
         int total=rows*cols;
+        int count=0;
         vector<int>ans;
         while(count<total){
-            for(int i=colst;i<=colend && count<total;i++){
-                ans.push_back(matrix[rowst][i]);
+            for(int j=colst;j<=colend && count<total;j++){
+                ans.push_back(matrix[rowst][j]);
                 count++;
             }
             rowst++;
-            for(int i=rowst;i<=rowend && count<total;i++){
-                ans.push_back(matrix[i][colend]);
+            for(int j=rowst;j<=rowend && count<total;j++){
+                ans.push_back(matrix[j][colend]);
                 count++;
             }
             colend--;
-            for(int i=colend;i>=colst && count<total;i--){
-                ans.push_back(matrix[rowend][i]);
+            for(int j=colend;j>=colst && count<total;j--){
+                ans.push_back(matrix[rowend][j]);
                 count++;
             }
             rowend--;
@@ -28,7 +28,7 @@ public:
                 ans.push_back(matrix[i][colst]);
                 count++;
             }
-            colst++; 
+            colst++;
         }
         return ans;
     }
