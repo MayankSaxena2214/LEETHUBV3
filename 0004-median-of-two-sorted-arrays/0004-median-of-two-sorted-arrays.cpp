7e569a9afd81e928f944468d1d04406c6f8f484e
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-11-04 18:20:16 +0530

    Time: 38 ms (5.15%), Space: 94.7 MB (87.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,69 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        if(n>m) return findMedianSortedArrays(nums2,nums1);
-        int l=0,r=n;
-        while(l<=r){
-            int Px=l+(r-l)/2;
-            int Py=(m+n+1)/2 -Px;
-
-            int x1=(Px==0)?INT_MIN:nums1[Px-1];
-            int y1=(Py==0)?INT_MIN:nums2[Py-1];
-            int x2=(Px==n)?INT_MAX:nums1[Px];
-            int y2=(Py==m)?INT_MAX:nums2[Py];
-
-            if(x1<=y2 && y1<=x2){
-                if((n+m)%2==0){
-                    return (max(x1,y1)+min(x2,y2))/2.0;
-                }
-                else{
-                    return (max(x1,y1));
-                }
-            }
-            else if(x1>y2){
-                r=Px-1;
+        int element1=0,element2=0;
+        int i=0,j=0,k=0;
+        int n=nums1.size(),m=nums2.size();
+        int idx1=(n+m)/2;
+        int idx2=idx1-1;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                if(k==idx1)element1=nums1[i];
+            if(k==idx2)element2=nums1[i];
+            i++;
+            k++;
             }
             else{
-                
-                l=Px+1;
+                if(k==idx1)element1=nums2[j];
+            if(k==idx2)element2=nums2[j];
+            j++;
+            k++;
             }
         }
-        return -1;
-        // int n=nums1.size();
-        // int m=nums2.size();
-        // if(n>m)return findMedianSortedArrays(nums2,nums1);
-
-        // int l=0;
-        // int r=n;
-        // while(l<=r){
-        //     int Px=(l+(r-l)/2);
-        //     int Py=(m+n+1)/2-Px;
-
-        //     //left vaale element
-        //     int x1=(Px==0)?INT_MIN:nums1[Px-1];
-        //     int y1=(Py==0)?INT_MIN:nums2[Py-1];
-        //     //right vaale
-        //     int x2=(Px==n)?INT_MAX:nums1[Px];
-        //     int y2=(Py==m)?INT_MAX:nums2[Py];
-
-        //     if(x1<=y2 && y1<=x2){
-        //         if((m+n)%2==0){
-        //             return (max(x1,y1)+min(x2,y2))/2.0;
-        //         }
-        //         else{
-        //             return max(x1,y1);
-        //         }
-        //     }
-        //     if(x1>y2){
-        //         r=Px-1;
-        //     }
-        //     else{
-        //         l=Px+1;
-        //     }
-        // }
-        // return -1;
-        
+        while(i<n){
+            if(k==idx1)element1=nums1[i];
+            if(k==idx2)element2=nums1[i];
+            i++;
+            k++;
+        }
+        while(j<m){
+            if(k==idx1)element1=nums2[j];
+            if(k==idx2)element2=nums2[j];
+            j++;
+            k++;
+        }
+        if((n+m)%2==0){
+            return ((element1+element2)/2.0);
+        }
+        return element1;
     }
 };
