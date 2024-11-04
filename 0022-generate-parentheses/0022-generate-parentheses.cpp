f339a24c6fb8f85046e518b0fd13c3d04a55a897
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-11-04 22:32:33 +0530

    Time: 3 ms (70.47%), Space: 16.1 MB (22.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void solve(auto&ans,auto&temp,int n,int open,int close){
+    void solve(auto&ans,auto temp,int open,int close,int n){
         if(open==n && close==n){
             ans.push_back(temp);
-            return;
+            return ;
         }
         if(open<n){
             temp.push_back('(');
-            solve(ans,temp,n,open+1,close);
+            solve(ans,temp,open+1,close,n);
             temp.pop_back();
         }
         if(close<open){
             temp.push_back(')');
-            solve(ans,temp,n,open,close+1);
+            solve(ans,temp,open,close+1,n);
             temp.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
+        int open=0;
+        int close=0;
         string temp;
-        solve(ans,temp,n,0,0);
+        vector<string>ans;
+        solve(ans,temp,open,close,n);
         return ans;
     }
 };
