f17799fa0d52a5bd270a53ae1e962d309e9a5b9b
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2025-03-27 09:28:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (82.5%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-        int m=x;
         int ans=0;
-        while(m){
-            int digit=m%10;
-            if(ans<INT_MIN/10 || ans>INT_MAX/10)return -1;
+        while(x!=0){
+            int digit=x%10;
+
+            if(ans<INT_MIN/10 || ans>INT_MAX/10)return 0;
             ans=ans*10+digit;
-            m=m/10;
+            x=x/10;
         }
         return ans;
     }
