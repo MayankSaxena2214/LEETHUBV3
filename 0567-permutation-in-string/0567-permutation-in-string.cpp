696036f74e78dd7ef3f6ab52bbb50f410506bd16
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2025-03-28 09:43:32 +0530

    Time: 1 ms (74.16%), Space: 10.1 MB (45.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,19 +3,22 @@ public:
     bool checkInclusion(string s1, string s2) {
         vector<int>count1(26,0);
         vector<int>count2(26,0);
-        int n=s1.length(),m=s2.length();
-        if(n>m)return false;
-        for(int i=0;i<n;i++){
+        if(s2.length()<s1.length())return false;
+        for(int i=0;i<s1.length();i++){
             count1[s1[i]-'a']++;
             count2[s2[i]-'a']++;
+
         }
         if(count1==count2)return true;
+        int n=s1.length();
         int i=n;
-        while(i<m){
+        
+        while(i<s2.length()){
+            //new char
+            count2[s2[i]-'a']++;
             //remove old char
             count2[s2[i-n]-'a']--;
-            count2[s2[i]-'a']++;
-            if(count2==count1)return true;
+            if(count1==count2)return true;
             i++;
         }
         return false;
