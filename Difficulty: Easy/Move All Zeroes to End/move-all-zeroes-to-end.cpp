9b2101423cbb1698ac5f8392261c3f8bf1493b1a
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2025-06-11 12:55:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int i=0;
+        for(int j=0;j<arr.size();j++){
+            if(arr[j]!=0){
+                swap(arr[j],arr[i]);
+                i++;
+            }
+        }
+        
+    }
+};
