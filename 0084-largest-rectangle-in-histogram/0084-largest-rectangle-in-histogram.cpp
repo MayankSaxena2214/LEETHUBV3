af4212101ee76135b6ff497bc7accac48af29984
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2025-07-06 12:26:47 +0530

    Time: 51 ms (12.28%), Space: 85.7 MB (37.91%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    void prevSmaller(auto&arr,auto&ans,auto&n){
-        stack<int>st;
-        st.push(-1);
-        for(int i=0;i<n;i++){
-            int curr=arr[i];
-            while(st.top()!=-1 && arr[st.top()]>=curr){
-                st.pop();
-            }
-            ans[i]=st.top();
-            st.push(i);
-        }
-    }
-    void nextSmaller(auto&arr,auto&ans,auto&n){
+    void nextSmaller(auto&arr,auto&next,auto&n){
         stack<int>st;
         st.push(-1);
         for(int i=n-1;i>=0;i--){
@@ -20,26 +8,39 @@ public:
             while(st.top()!=-1 && arr[st.top()]>=curr){
                 st.pop();
             }
-            ans[i]=st.top();
+            next[i]=st.top();
             st.push(i);
         }
+
+    }
+    void prevSmaller(auto&arr,auto&prev,auto&n){
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<n;i++){
+            int curr=arr[i];
+            while(st.top()!=-1 && arr[st.top()]>=curr){
+                st.pop();
+            }
+            prev[i]=st.top();
+            st.push(i);
+        }
+        
     }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int>prev(n);
-        vector<int>next(n);
+        vector<int>next(n),prev(n);
         prevSmaller(heights,prev,n);
         nextSmaller(heights,next,n);
 
-        int area=-1;
+        int area=INT_MIN;
         for(int i=0;i<n;i++){
-            int b=heights[i];
+            int l=heights[i];
             if(next[i]==-1){
                 next[i]=n;
-
             }
-            int l=next[i]-prev[i]-1;
+            int b=next[i]-prev[i]-1;
             area=max(area,l*b);
+
         }
         return area;
     }
