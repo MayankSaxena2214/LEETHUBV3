2d5131ea61c87f648e9380c4cf96b114cd9a6c70
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2025-10-27 12:06:09 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
-    int firstOccur(vector<int>& nums, int target) {
-        int s=0,e=nums.size()-1;
+    int firstOccur(auto&nums,auto&target,int&n){
         int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
+        int low=0,high=n-1;
+        int mid=(low+(high-low)/2);
+
+        while(low<=high){
             if(nums[mid]==target){
                 ans=mid;
-                e--;
+                high=mid-1;
             }
-            else if(nums[mid]>target)e--;
-            else s++;
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            mid=(low+(high-low)/2);
+            
         }
         return ans;
     }
-    int lastOccur(vector<int>& nums, int target) {
-        int s=0,e=nums.size()-1;
+    int lastOccur(auto&nums,auto&target,int&n){
         int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
+        int low=0,high=n-1;
+        int mid=(low+(high-low)/2);
+
+        while(low<=high){
             if(nums[mid]==target){
                 ans=mid;
-                s++;
+                low=mid+1;
             }
-            else if(nums[mid]>target)e--;
-            else s++;
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            mid=(low+(high-low)/2);
+            
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=firstOccur(nums,target);
-        int last=lastOccur(nums,target);
+        int n=nums.size();
+        int first=firstOccur(nums,target,n);
+        int last=lastOccur(nums,target,n);
+
         return {first,last};
     }
 };
